89fbc020d2f4bc4a3fccb3bf0343ce63f9821182
rommelnita <93637393+rommelnita@users.noreply.github.com>
2022-03-27 03:37:44 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ async def man_userbot_on():
     except Exception as e:
         LOGS.info(str(e))
     try:
-        await bot(JoinChannelRequest("@Lunatic0de"))
+        await bot(JoinChannelRequest("@Jual_Nokos_Telegram"))
     except BaseException:
         pass
     try:
